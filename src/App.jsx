221f663af6d5b62730eb7fdd05b8e7df5eb7dff2
michael5671 <giaphu.gm04@gmail.com>
2025-07-06 00:22:13 +0700

    add component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-//component = html + css + js
-const MyComponent = () => {
-  return (
-    <div>Gia phu nek update</div> //tag
-  );
-}
+import MyComponent from './components/learn/MyComponent'
+import { SecondComponent, ThirdComponent } from './components/learn/SecondComponent'
 
 const App = () => {
   const [count, setCount] = useState(0)
@@ -26,7 +21,9 @@ const App = () => {
       <h1>Hello world !</h1>
 
       <MyComponent/>
-      
+      <SecondComponent/>
+      <ThirdComponent/>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,9 @@
+//component = html + css + js
+
+const MyComponent = () => {
+  return (
+    <div>Gia phu nek update</div> //tag
+  );
+}
+
+export default MyComponent;
