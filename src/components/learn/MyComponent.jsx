951a21fcaf8b02a7052972932cc2d7699e8f4192
michael5671 <giaphu.gm04@gmail.com>
2025-07-06 12:42:46 +0700

    jsx

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -5,11 +5,27 @@
 import './style.css';
 
 const MyComponent = () => {
+  // const giaphu = "phu 1"; //string
+  // const giaphu = 25;
+  // const giaphu = true;
+  // const giaphu = undefined;
+  // const giaphu = null;
+
+  const giaphu = [1, 2, 3]
+  // const giaphu = {
+  //   name : "giaphu",
+  //   age: 25
+  // }
+
+
   return (
     <>
-        <div className="">Gia phu nek update</div> 
+        <div className="">{JSON.stringify(giaphu)} & Gia phu nek update</div> 
+        <div>{console.log("gia phu")}</div>
         <div className="child" 
-            style={{borderRadius: "10px"}}
+            style={
+              {borderRadius: "10px"}
+            }
         >Child</div>
     </>
   );
