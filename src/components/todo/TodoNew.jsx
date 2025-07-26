c4e3dfc90e42f14f5e428f9b44f1aac67ce1e9b7
michael5671 <giaphu.gm04@gmail.com>
2025-07-26 22:27:39 +0700

    handle event

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -2,12 +2,23 @@ const TodoNew = (props) => {
     console.log(props);
     const { addNewTodo } = props;
 
-    // addNewTodo("giaphu");
+    // addNewTodo("giaphu") // fire
+    const handleClick = () => {
+        alert("click me")
+    }
 
+    const handleOnChange = (name) => {
+        console.log("handleOnChange", name);
+    }
     return (
         <div className="todo-new" >
-            <input type="text"></input>
-            <button >Add</button>
+            <input type="text"
+                onChange={ (event) => handleOnChange(event.target.value)}
+            ></input>
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
